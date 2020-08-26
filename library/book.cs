0c4e4f9fb42effe081e4da2aa218d11f26ccf850
mathias kamp <mathias.kamp@hotmail.com>
2020-08-26 21:03:05 +0200

    Add project files.

---
+++ library/book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bibliotek
+{
+
+
+    /// <summary>
+    /// class book
+    /// attributes, name, yearReleased, bookSize, releaseNumber
+    /// constructoren tager alle mine attributer med til når der bliver oprettet et nyt
+    /// objekt af typen book.
+    /// </summary>
+    class book
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int yearReleased;
+
+        public int YearReleased
+        {
+            get { return yearReleased; }
+            set { yearReleased = value; }
+        }
+
+        private int bookSize;
+
+        public int BookSize
+        {
+            get { return bookSize; }
+            set { bookSize = value; }
+        }
+
+        private int releaseNumber;
+
+        public int ReleaseNumber
+        {
+            get { return releaseNumber; }
+            set { releaseNumber = value; }
+        }
+
+
+
+        public book(string name, int yearreleased, int booksize, int number)
+        {
+            this.bookSize = booksize;
+            this.name = name;
+            this.yearReleased = yearreleased;
+            this.releaseNumber = number;
+
+        }
+
+    }
+}
---
+++ library/gui.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace bibliotek
+{
+
+    /// <summary>
+    /// Denne klasse har en metode renderMenu() som udskriver min menu
+    /// </summary>
+    class gui
+    {
+        public void renderMenu()
+        {
+           
+            Console.WriteLine("=============================================");
+            Console.WriteLine("                   library                   ");
+            Console.WriteLine("=============================================");
+
+            Console.WriteLine("1. Show books available");
+            Console.WriteLine("2. Loan a book");
+            Console.WriteLine("3. Show your loans");
+            Console.WriteLine("4. Complete loan");
+            Console.WriteLine("5. Exit");
+        }
+    
+    }
+}
